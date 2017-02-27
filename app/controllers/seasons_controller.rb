424bdbea84ee0884acab5685b4d98bb0f9a0f9ed
Samuel Clements <sdkclements@gmail.com>
2017-02-27 16:37:20 -0500

    methods for seasons

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -1,13 +1,34 @@
 class SeasonsController < ApplicationController
-  def index
-  end
+  
 
-  def show
+   def index
+  	@seasons = Season.all
   end
 
   def new
+  	@season = Season.new
   end
 
+  def create
+    @season = Season.create!(params[:season])
+  end
+
+  def season
+  	@season = Season.find(params[:id])
+  end
   def edit
+  @season = Season.find(params[:id])
+  end
+
+
+  def update
+    @season = Season.update(params[:id])
+    redirect_to season_path
+  end
+
+  def destroy
+  @season = Season.find(params[:id])
+  @season.destroy
+  redirect_to :root
   end
 end
