56aa962c743b04626f696bbce0599e0d630f972f
Samuel Clements <sdkclements@gmail.com>
2017-02-27 15:19:00 -0500

    maybe nesting routes bad idea here reverting back

--- config/routes.rb
+++ config/routes.rb
@@ -23,11 +23,10 @@ Rails.application.routes.draw do
 
   # get 'shows/edit'
 
-  resources :shows do 
-    resources :seasons do
-      resources :episodes
-    end
-  end
+  resources :shows 
+  resources :seasons 
+  resources :episodes
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
