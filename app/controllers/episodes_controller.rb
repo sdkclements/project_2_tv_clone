154c1169fcf8b5778fec254db86e40cb976a5da8
Samuel Clements <sdkclements@gmail.com>
2017-02-27 16:03:01 -0500

    added crud to each controller, next views

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,13 +1,34 @@
 class EpisodesController < ApplicationController
-  def index
-  end
 
-  def show
+
+   def index
+  	@episodes = Episode.all
   end
 
   def new
+  	@episode = Episode.new
   end
 
+  def create
+    @episode = Episode.create!(params[:episode])
+  end
+
+  def episode
+  	@episode = Episode.find(params[:id])
+  end
   def edit
+  @episode = Episode.find(params[:id])
+  end
+
+
+  def update
+    @episode = Episode.update(params[:id])
+    redirect_to episode_path
+  end
+
+  def destroy
+  @episode = Episode.find(params[:id])
+  @episode.destroy
+  redirect_to :root
   end
 end
