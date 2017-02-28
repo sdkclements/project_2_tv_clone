8f50c8bb48bc3e795ce7ae60dd5594b0f581dddf
Samuel Clements <sdkclements@gmail.com>
2017-02-28 08:47:05 -0500

    thoughts for seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
+
+# drop_table(:users, if_exists: true) in 
+# a migration ensures that the migration will run without exceptions, whether or not the “users” table exists.
+
+
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
